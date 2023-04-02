1e0b07d34e210e616622866a8c7a45a9e8429bbd
eardakbekov1 <eardakbekov@gmail.com>
2023-04-03 03:22:14 +0600

    relations_done

--- app/Models/HallMovieSeatSession.php
+++ app/Models/HallMovieSeatSession.php
@@ -7,5 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class HallMovieSeatSession extends Model
 {
-    use HasFactory;
+    protected $fillable = ['id', 'hall_id', 'movie_id', 'seat_id', 'session_id', 'user_id'];
+    public function hall(): BelongsTo
+    {
+        return $this->belongsTo(Hall::class);
+    }
+
+    public function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    public function seat(): BelongsTo
+    {
+        return $this->belongsTo(Seat::class);
+    }
+
+    public function session(): BelongsTo
+    {
+        return $this->belongsTo(Session::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
